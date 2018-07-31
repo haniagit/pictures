ccfc07c4c016fb405e64b946edd71722d01c7bd3
Hania <haniagit08@gmail.com>
2018-07-31 09:25:05 +0200

    first init

---
+++ src/main/java/com/example/obrazki/ObrazkiApplication.java
@@ -0,0 +1,12 @@
+package com.example.obrazki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObrazkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObrazkiApplication.class, args);
+	}
+}
