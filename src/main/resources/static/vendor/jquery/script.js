9d89c81f89ecf4ada8a3f616eb951fc720926404
Hania <haniagit08@gmail.com>
2018-08-02 10:53:32 +0200

    gif-details page - the star indicating favorite gifs is now clickable

--- src/main/resources/static/vendor/jquery/script.js
+++ src/main/resources/static/vendor/jquery/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-    $("img").click(function () {
-        alert("The image was clicked.");
+    $('div div div a').click( function(){
+        if ( $(this).hasClass('mark favorite') ) {
+            $(this).removeClass('mark favorite');
+            $(this).addClass('unmark favorite');
+        } else {
+            $('div div div a').removeClass('unmark favorite');
+            $(this).addClass('mark favorite');
+        }
     });
-
-});
+})
